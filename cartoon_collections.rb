30b8644e325fb0cca53f8009b8350274d0930aec
Alina Kornilova <alinakornilova78@gmail.com>
2019-09-23 09:03:43 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,17 +6,38 @@ puts "#{index+1}. #{name}"
 end 
 end 
 
-# roll_call_dwarves(dwarves)
+roll_call_dwarves(dwarves)
 
-def summon_captain_planet# code an argument here
-  # Your code here
-end
+planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
 
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
+def summon_captain_planet(array)
+array.collect do |word| 
+word.capitalize + "!"
+end 
+end 
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
-end
+summon_captain_planet(planeteer_calls)
+
+short_words = ["puff", "go", "two"]
+assorted_words = ["two", "go", "industrious", "bop"]
+
+def long_planeteer_calls(array)
+array.any? do |word| 
+word.length > 4 
+end 
+end 
+
+long_planeteer_calls(short_words)
+long_planeteer_calls(assorted_words)
+
+snacks = ["crackers", "gouda", "thyme"]
+soup = ["tomato soup", "cheddar", "oyster crackers", "gouda"]
+ingredients = ["garlic", "bread", "rosemary"]
+
+def find_the_cheese(array)
+  array.find do |cheese| 
+    cheese == "cheddar" || cheese == "gouda" || cheese == "camembert"
+    end 
+  end 
+  
+find_the_cheese(ingredients)
